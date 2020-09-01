9353763ef60e6933536d12ffa4455e252065928a
Xearta <rsmcwow@gmail.com>
2020-09-01 12:51:48 -0400

    Added a custom picker and text input

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
@@ -11,7 +11,28 @@ import Icon from './app/components/Icon';
 import ListItem from './app/components/ListItem';
 import AccountScreen from './app/screens/AccountScreen';
 import ListingsScreen from './app/screens/ListingsScreen';
+import AppTextInput from './app/components/AppTextInput';
+import AppPicker from './app/components/AppPicker';
+
+const categories = [
+  { label: 'Furniture', value: 1 },
+  { label: 'Clothing', value: 2 },
+  { label: 'Cameras', value: 3 },
+];
 
 export default function App() {
-  return <ListingsScreen />;
+  const [category, setCategory] = useState(categories[0]);
+
+  return (
+    <Screen>
+      <AppTextInput icon='email' placeholder='Username' />
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={item => setCategory(item)}
+        icon='apps'
+        items={categories}
+        placeholder='Category'
+      />
+    </Screen>
+  );
 }
